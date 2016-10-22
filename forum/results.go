e2f1421c454de3cbb5daf766bad57cc384b4a26d
Ilya Timofeev <ilya.timofeev@ru.axxonsoft.com>
2016-10-22 17:19:43 +0300

    #1 Ссылки на профили vk * struct for all fields

--- forum/results.go
+++ forum/results.go
@@ -1,29 +1,47 @@
 package forum
 
 type ForumResults struct {
-	JudgesResults []*JudgeResult
-
-	Stages          []string `json:"-"`
-	TechStages      []string `json:"-"`
-	FinalTechStages []string `json:"-"`
-	TechResult      []string `json:"-"`
-	FinalTechResult []string `json:"-"`
-	Places          []string `json:"-"`
+	JudgesResults []*Judge
 }
 
-type JudgeResult struct {
+type Judge struct {
 	Judges      []string
-	Nominations []*NominationResult
+	Nominations []*Nomination
 }
 
-type NominationResult struct {
-	Title string
+type Nomination struct {
+	Title           string
+	Stages          []*Stage
+	TechStages      []*TechStage
+	FinalTechStage  string
+	FinalTechResult []*FinalTechResult
+}
+
+type Places struct {
+	Value string
+}
+
+type Stage struct {
+	Title  string
+	Places []*Places
+}
+
+type TechStage struct {
+	Title   string
+	Results []*TechResult
+}
+
+type TechResult struct {
+	Value string
+}
+type FinalTechResult struct {
+	Value string
 }
 
 func (fr *ForumResults) addJudge(line string) {
 	needNew := len(fr.JudgesResults) == 0 || len(fr.JudgesResults[len(fr.JudgesResults)-1].Nominations) > 0
 	if needNew {
-		fr.JudgesResults = append(fr.JudgesResults, &JudgeResult{
+		fr.JudgesResults = append(fr.JudgesResults, &Judge{
 			Judges: []string{line},
 		})
 	} else {
@@ -35,33 +53,62 @@ func (fr *ForumResults) addJudge(line string) {
 func (fr *ForumResults) addNominationName(line string) {
 	lastJudgeResult := fr.JudgesResults[len(fr.JudgesResults)-1]
 
-	lastJudgeResult.Nominations = append(lastJudgeResult.Nominations, &NominationResult{
+	lastJudgeResult.Nominations = append(lastJudgeResult.Nominations, &Nomination{
 		Title: line,
 	})
 }
 
 func (fr *ForumResults) addStage(line string) {
-	fr.Stages = append(fr.Stages, line)
-}
+	lastJudgeResult := fr.JudgesResults[len(fr.JudgesResults)-1]
+	lastNomination := lastJudgeResult.Nominations[len(lastJudgeResult.Nominations)-1]
 
-func (fr *ForumResults) addTechStage(line string) {
-	fr.TechStages = append(fr.TechStages, line)
-}
-
-func (fr *ForumResults) addFinalTechStage(line string) {
-	fr.FinalTechStages = append(fr.FinalTechStages, line)
-}
-
-func (fr *ForumResults) addTechResult(line string) {
-	// 658   | AD|E   ==> выход в 1/4 финала ||      672   | BE|   место: 23-30
-	fr.TechResult = append(fr.TechResult, line)
-}
-
-func (fr *ForumResults) addFinalTechResult(line string) {
-	//  687   | 1 5 4 2 6         │     5  │ 4 4 3 2 5         │     4  │                   │        │    9 │    4
-	fr.FinalTechResult = append(fr.FinalTechResult, line)
+	lastNomination.Stages = append(lastNomination.Stages, &Stage{
+		Title: line,
+	})
 }
 
 func (fr *ForumResults) addPlace(line string) {
-	fr.Places = append(fr.Places, line)
+	lastJudgeResult := fr.JudgesResults[len(fr.JudgesResults)-1]
+	lastNomination := lastJudgeResult.Nominations[len(lastJudgeResult.Nominations)-1]
+	lastStage := lastNomination.Stages[len(lastNomination.Stages)-1]
+
+	lastStage.Places = append(lastStage.Places, &Places{
+		Value: line,
+	})
+}
+
+func (fr *ForumResults) addTechStage(line string) {
+	lastJudgeResult := fr.JudgesResults[len(fr.JudgesResults)-1]
+	lastNomination := lastJudgeResult.Nominations[len(lastJudgeResult.Nominations)-1]
+
+	lastNomination.TechStages = append(lastNomination.TechStages, &TechStage{
+		Title: line,
+	})
+}
+
+// 658   | AD|E   ==> выход в 1/4 финала ||      672   | BE|   место: 23-30
+func (fr *ForumResults) addTechResult(line string) {
+	lastJudgeResult := fr.JudgesResults[len(fr.JudgesResults)-1]
+	lastNomination := lastJudgeResult.Nominations[len(lastJudgeResult.Nominations)-1]
+	lastTechStage := lastNomination.TechStages[len(lastNomination.TechStages)-1]
+
+	lastTechStage.Results = append(lastTechStage.Results, &TechResult{
+		Value: line,
+	})
+}
+
+func (fr *ForumResults) addFinalTechStage(line string) {
+	lastJudgeResult := fr.JudgesResults[len(fr.JudgesResults)-1]
+	lastNomination := lastJudgeResult.Nominations[len(lastJudgeResult.Nominations)-1]
+	lastNomination.FinalTechStage = line
+}
+
+//  687   | 1 5 4 2 6         │     5  │ 4 4 3 2 5         │     4  │                   │        │    9 │    4
+func (fr *ForumResults) addFinalTechResult(line string) {
+	lastJudgeResult := fr.JudgesResults[len(fr.JudgesResults)-1]
+	lastNomination := lastJudgeResult.Nominations[len(lastJudgeResult.Nominations)-1]
+
+	lastNomination.FinalTechResult = append(lastNomination.FinalTechResult, &FinalTechResult{
+		Value: line,
+	})
 }
