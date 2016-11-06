22ac366294b868a95c8b8b82bf4a6a1239961a02
Ilya Timofeev <ilya.timofeev@ru.axxonsoft.com>
2016-11-06 17:49:09 +0300

    Список предварительной регистрации на турниры #7 * parse prereg id and forum url from main page

---
+++ forum/prereg/prereg_extractor.go
@@ -0,0 +1,91 @@
+package prereg
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/itimofeev/hustledb/util"
+	"strings"
+)
+
+type PreregComp struct {
+	ID            int64              `json:"id" db:"id"`
+	PreregId      int                `json:"prereg_id" db:"prereg_id"`
+	CompetitionId string             `json:"competition_id" db:"competition_id"`
+	Nominations   []PreregNomination `json:"nominations"`
+}
+
+type PreregNomination struct {
+	ID           int64  `json:"id" db:"id"`
+	PreregCompId int64  `json:"prereg_comp_id" db:"prereg_comp_id"`
+	Title        string `json:"title" db:"title"`
+	Records      string `json:"records"`
+}
+
+type PreregRecord struct {
+	ID          int64 `json:"id" db:"id"`
+	PreregNomId int64 `json:"prereg_nom_id" db:"prereg_nom_id"`
+	Index       int   `json:"index" db:"idnex"`
+}
+
+type PreregDancer struct {
+	ID      int64        `json:"id" db:"id"`
+	CodeASH *string      `json:"code_ash" db:"code_ash"`
+	Class   string       `json:"class" db:"class"`
+	Title   string       `json:"title" db:"title"`
+	Clubs   []PreregClub `json:"clubs"`
+}
+
+type PreregClub struct {
+	ID    int64  `json:"id" db:"id"`
+	Title string `json:"title" db:"title"`
+}
+
+const mainUrl = "http://www.liveindance.ru/"
+const regUrl = "http://www.liveindance.ru/contest/reg.php?id=%d"
+
+func ParseAllPreregLinks() []string {
+	data := util.GetUrlContent(mainUrl)
+	doc, err := goquery.NewDocumentFromReader(bytes.NewBuffer(data))
+	util.CheckErr(err)
+
+	var listLinks []string
+
+	doc.
+		Find(".panel-body a").
+		FilterFunction(func(i int, s *goquery.Selection) bool {
+			link := s.AttrOr("href", "")
+			return strings.Contains(link, "http://www.liveindance.ru/contest/registration/list.php?id=")
+		}).
+		Each(func(i int, s *goquery.Selection) {
+			link := s.AttrOr("href", "")
+			listLinks = append(listLinks, link)
+		})
+	return listLinks
+}
+
+func ParsePreregId(listLink string) int {
+	preregId := strings.Replace(listLink, "http://www.liveindance.ru/contest/registration/list.php?id=", "", 1)
+
+	return util.Atoi(preregId)
+}
+
+func GetForumCompetitionId(preregId int) string {
+	regUrlFull := fmt.Sprintf(regUrl, preregId)
+	data := util.GetUrlContent(regUrlFull)
+
+	doc, err := goquery.NewDocumentFromReader(bytes.NewBuffer(data))
+	util.CheckErr(err)
+
+	forumLinkA := doc.
+		Find("table table table tr a").
+		FilterFunction(func(i int, s *goquery.Selection) bool {
+			return strings.Contains(s.AttrOr("href", ""), "http://hustle-sa.ru/forum/index.php?showtopic=")
+		}).First()
+
+	return forumLinkA.AttrOr("href", "")
+}
+
+func ParsePreregCompetition(preregId int, fCompUrl string) PreregComp {
+ return PreregComp{}
+}
--- main/forum_comp/main.go
+++ main/forum_comp/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
-	"github.com/itimofeev/hustledb/forum"
+	"github.com/itimofeev/hustledb/forum/prereg"
 	"github.com/itimofeev/hustledb/util"
 )
 
 func main() {
-	util.PrintJson(forum.ParseCompetitionsFromForum())
+	f()
+}
+
+func f() {
+	listLinks := prereg.ParseAllPreregLinks()
+	util.PrintJson(listLinks)
+
+	var ids []int
+	for _, listLink := range listLinks {
+		ids = append(ids, prereg.ParsePreregId(listLink))
+	}
+	util.PrintJson(ids)
+
+	var fCompUrls []string
+	for _, preregId := range ids {
+		fCompUrl := prereg.GetForumCompetitionId(preregId)
+		fCompUrls = append(fCompUrls, fCompUrl)
+	}
+
+	util.PrintJson(fCompUrls)
 }
